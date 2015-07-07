a67bd20b32f6411543a77306aee475df0762f702
lasse-abelsen <lqa@kmd.dk>
2015-07-07 13:17:21 +0200

    First Commit

---
+++ Test1/Teacher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    public class Teacher : Person
+    {
+        public Teacher(string fname, string lname, string cmail = "No mail")
+            :base(fname, lname, cmail)
+        {
+            ID = Guid.NewGuid();
+            FirstName = fname;
+            LastName = lname;
+            ContactMail = cmail;
+        }
+
+        public List<ClassDef> ClassList { get; set; }
+
+        //Guid = unique identifier, se fx https://msdn.microsoft.com/en-us/library/system.guid.newguid(v=vs.110).aspx
+        private Guid ID;
+    }
+}
